e09043990cb2ee0c8a4c39b9e88bfa3b8053f8c5
Patel <krunalpa@amazon.com>
2017-03-14 10:49:51 -0700

    Added Crime form
    Added fragment to show the crime form
    
    Added Crime Model
    
    Attached the Crime Fragment with the Crime Activity

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,8 +1,48 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by krunalpa on 3/14/17.
  */
 
 public class Crime {
+    private UUID id;
+    private String title;
+    private Date date;
+    private boolean solved;
+
+    public Crime() {
+        id = UUID.randomUUID();
+        date = new Date();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean getSolved() {
+        return solved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.solved = solved;
+    }
 }
