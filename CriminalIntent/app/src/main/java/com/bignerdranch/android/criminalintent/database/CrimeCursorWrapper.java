acb0c4337455ce1d99e538e4dc144927d5785c33
Patel <krunalpa@amazon.com>
2017-03-15 13:15:36 -0700

    Introduced SQLite database
    Added SQLite wiring and wrappers to store, update and query crimes from the db

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,39 @@
+package com.bignerdranch.android.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.bignerdranch.android.criminalintent.Crime;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by krunalpa on 3/15/17.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+    /**
+     * Creates a cursor wrapper.
+     *
+     * @param cursor The underlying cursor to wrap.
+     */
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int solved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(solved != 0);
+
+        return crime;
+    }
+}
