a37e48bcd5ac57750393d38c2ef0836913302b41
Patel <krunalpa@amazon.com>
2017-03-16 11:22:43 -0700

    Introduced ASync tasks for multithreading
    Async tasks handle the http connection to flickr to get the images metadata
    
    Using Recyclerview and plumbed View Holder and Adapter for recyclerview

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -8,6 +8,6 @@ public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return null;
+        return PhotoGalleryFragment.newInstance();
     }
 }
