90a7f3dae9429cc9b0f2e1813133695c80dfcec0
Patel <krunalpa@amazon.com>
2017-03-15 18:13:54 -0700

    Introduced Photo in the app
    Used Camera Intent to call out the camera app and worked around the permissions.

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -11,6 +11,7 @@ import com.bignerdranch.android.criminalintent.database.CrimeCursorWrapper;
 import com.bignerdranch.android.criminalintent.database.CrimeDbSchema;
 import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -99,6 +100,11 @@ public class CrimeLab {
         );
 
         return new CrimeCursorWrapper(cursor);
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
     }
 
     private static ContentValues getContentValues(Crime crime) {
