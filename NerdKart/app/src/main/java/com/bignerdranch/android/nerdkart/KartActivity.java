fdeca484956985d8c0dd8004ba7ecf6a5d2078e2
Patel <krunalpa@amazon.com>
2017-03-16 16:02:13 -0700

    Added NerdKart project

---
+++ NerdKart/app/src/main/java/com/bignerdranch/android/nerdkart/KartActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.nerdkart;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KartActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
---
+++ NerdKart/app/src/main/java/com/bignerdranch/android/nerdkart/KartFragment.java
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.nerdkart;
+
+/**
+ * Created by krunalpa on 3/16/17.
+ */
+
+public class KartFragment {
+}
