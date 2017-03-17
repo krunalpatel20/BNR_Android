0559e6f4724f872bf5be1efb1462f63c5817e01f
Patel <krunalpa@amazon.com>
2017-03-17 09:46:20 -0700

    Added DragAndDraw project
    Project to learn touch events

---
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/DranAndDrawFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by krunalpa on 3/17/17.
+ */
+
+public class DranAndDrawFragment extends Fragment {
+}
